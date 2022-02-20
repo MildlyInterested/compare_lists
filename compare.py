b7b1d83c8b30c4075ad808dfaba98dfb011bc438
Mildly_Interested <mueller.manuel@mailbox.org>
2022-02-20 15:24:27 +0100

    v1

--- compare.py
+++ compare.py
@@ -1,48 +1,23 @@
-import tkinter as tk
+import streamlit as st
 
-root = tk.Tk()
-
-list_1_var=tk.StringVar()
-list_2_var=tk.StringVar()
+st.title('Mod Whitelisting Utility')
+info_text_1 = st.write('1. execute "copyToClipboard str ("true" configClasses (configFile >> "CfgPatches") apply {configName _x});" locally in the editor')
+info_text_2 = st.write('2. CTRL+V in the following boxes (may freeze for a second)')
+input_str_1 = st.text_input("Loaded Addons no 1", value='None')
+input_str_2 = st.text_input("Loaded Addons no 2", value='None')
 
 # compare difference between two lists and output the diff
 def get_diff():
-    display_optionals.delete(0, tk.END)
-    list_1 = list_1_var.get().split(',')
-    list_2 = list_2_var.get().split(',')
-    diff = set(list_1) ^ set(list_2)
-    if len(diff)==0:
-        diff = 'No differences found!'
+    list_1 = str(input_str_1).replace(' ','').split(',') #remove leading space from text input and make list
+    list_2 = str(input_str_2).replace(' ','').split(',')
+    diff = set(list_1) ^ set(list_2) #compare sets of each list and find differences
+    diff_no_empty = list(filter(None, diff)) #remove empty entries and make sure it stays as list
+    if len(diff_no_empty)==0:
+        diff_str = 'No differences found!'
     else:
-        diff_str='['+str(diff).strip('{}').replace(' ','').replace('"','')+']'
-        display_optionals.insert(0, diff_str)
-    
-    
+        diff_str= str(diff_no_empty).strip('{}').replace(' ','').replace('"','')
+    return diff_str
 
-#TEXT
-info_text_1 = '1. execute "copyToClipboard str ("true" configClasses (configFile >> "CfgPatches") apply {configName _x});" locally in the editor'
-info_text_2 = '2. CTRL+V in the following boxes (may freeze for a second)'
-mod_1_text = 'Loaded addons no 1'
-mod_2_text = 'Loaded addons no 2'
+diff_str = get_diff()
 
-#GUI
-root.title('Mildly_Interested')
-window_label = tk.Label(root, text = "Mod Whitelisting Utility", font=('Arial', 14)).grid(row=0, columnspan=2)
-
-info_1 = tk.Label(root, text=info_text_1).grid(row=1, column=0, columnspan=2)
-info_2 = tk.Label(root, text=info_text_2).grid(row=2, column=0, columnspan= 2)
-
-mod_1 = tk.Label(root, text=mod_1_text).grid(row=3, column=0)
-input_mod_list_1 = tk.Entry(root, textvariable=list_1_var).grid(row=3, column=1)
-
-mod_2 = tk.Label(root, text=mod_2_text).grid(row=4, column=0)
-input_mod_list_2 = tk.Entry(root, textvariable=list_2_var).grid(row=4, column=1)
-
-tk.Button(text='Find Difference', command=get_diff).grid(row=5, column=1)
-
-optionals_1 = tk.Label(root, text='Optional addons:').grid(row=6, column=0)
-display_optionals = tk.Entry(root)
-display_optionals.grid(row=6, column= 1)
-display_optionals.insert(0, 'No differences found!')
-
-root.mainloop()
+output_str = st.text(diff_str)
